b66ff1f6ff83f1f97ccfd054557377cd64a4de71
Paul_RazDva <paul1312white@gmail.com>
2022-12-03 16:52:27 +0300

    junit4

---
+++ src/test/java/ru/netology/unit/CashBackHackerServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.unit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashBackHackerServiceTest {
+
+    @Test
+    public void remain() {
+        CashBackHackerService service = new CashBackHackerService();
+        int actual = 900;
+        int expected = service.remain(100);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void test1000() {
+        CashBackHackerService service = new CashBackHackerService();
+        int actual = 1000;
+        int expected = service.remain(0);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testOne() {
+        CashBackHackerService service = new CashBackHackerService();
+        int actual = 1;
+        int expected = service.remain(999);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testOneThousand() {
+        CashBackHackerService service = new CashBackHackerService();
+        int actual = 1000;
+        int expected = service.remain(0);
+
+        assertEquals(actual, expected);
+    }
+}
